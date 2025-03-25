d6a94d65aac60d2615262b604fac6d639fd110a0
antonriabokon <s2500139@edu.bc.fi>
2025-03-25 09:52:24 +0200

    react one testing

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Good morning, good morning,<br />
+        Good morning to you!<br />
+        Good morning good morning,<br />
+        I am glad to see you!😉🤗<br />
+      </p>
+    </>
+  )
+}
+
+export default App
